7c416635b3124f9bdac454332bda3da66d4ebc59
Chris Wolfe <chriswwolfe@gmail.com>
2016-07-02 08:01:54 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to firedamp
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,23 @@
+(defproject firedamp "0.1.0-SNAPSHOT"
+  :description "Bot that reports that status of Github, Codecov, and Travis APIs"
+  :url "https://gitub.com/derwolfe/firedamp"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.taoensso/timbre "4.5.1"]
+                 [aleph "0.4.1"]
+                 [manifold "0.1.4"]
+                 [clj-time "0.12.0"]
+                 [cheshire "5.6.3"]
+                 [com.gfredericks/system-slash-exit "0.2.0"]
+                 [com.cemerick/url "0.1.1"]]
+  :plugins [[lein-auto "0.1.2"]
+            [lein-cljfmt "0.3.0"]
+            [lein-pprint "1.1.1"]
+            [lein-environ "1.0.2"]
+            [lein-ancient "0.6.10"]
+            [jonase/eastwood "0.2.3"]
+            [lein-cloverage "1.0.7-SNAPSHOT"]]
+  :main ^:skip-aot firedamp.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
