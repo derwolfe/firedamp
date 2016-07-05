2498154ad26b8b6e7108e60fac6f4032acc4138e
Chris Wolfe <chriswwolfe@gmail.com>
2016-07-05 15:14:53 -0500

    ... no tests, working

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [environ "1.0.3"]
                  [com.taoensso/timbre "4.5.1"]
                  [adamwynne/feedparser-clj "0.5.2"]
                  [aleph "0.4.1"]
@@ -11,10 +12,12 @@
                  [clj-time "0.12.0"]
                  [cheshire "5.6.3"]
                  [byte-streams "0.2.2"]
+                 [twitter-api "0.7.8"]
                  [com.gfredericks/system-slash-exit "0.2.0"]
                  [com.cemerick/url "0.1.1"]]
   :plugins [[lein-auto "0.1.2"]
             [lein-cljfmt "0.3.0"]
+            [lein-environ "1.0.3"]
             [lein-pprint "1.1.1"]
             [lein-environ "1.0.2"]
             [lein-ancient "0.6.10"]
--- src/firedamp/core.clj
+++ src/firedamp/core.clj
@@ -1,44 +1,118 @@
 (ns firedamp.core
-  (:require [byte-streams :as bs]
-            [feedparser-clj.core :as feedparser]
-            [clj-time.core :as time]
-            [clj-time.coerce :as time-coerce]
-            [cheshire.core :as json])
+  (:require
+   [aleph.http :as http]
+   [byte-streams :as bs]
+   [cheshire.core :as json]
+   [clj-time.core :as time]
+   [clj-time.coerce :as time-coerce]
+   [clojure.core.reducers :as r]
+   [environ.core :as env]
+   [feedparser-clj.core :as feedparser]
+   [manifold.deferred :as md]
+   [manifold.time :as mt]
+   [taoensso.timbre :as timbre]
+   [twitter.oauth :as tw-auth]
+   [twitter.api.restful :as tw-api])
   (:gen-class))
+
+(def github "https://status.github.com/api/status.json")
+(def codecov "http://status.codecov.io/history.atom")
+(def travis "http://www.traviscistatus.com/history.atom")
+
+(def state (atom false))
 
 (defn parse-status-page
   [feed threshold]
   (let [parsed (feedparser/parse-feed feed)
-        ;; get the most recent entries within threshold
         entries (:entries parsed)
-        within-threshold (filter #(time/after? (time-coerce/from-date (:updated-date %)) threshold) entries)]
+        within-threshold  (filter
+                           #(time/after? (time-coerce/from-date (:updated-date %)) threshold)
+                           entries)]
     within-threshold))
 
 (defn parse-github
   [msg]
-  (:status (json/parse-string msg true)))
+  (:status msg))
 
-(defn ^:private fetch-statuspage-io
+(defn fetch-statuspage
   [url threshold]
-  nil)
-
-(defn fetch-travis-status
-  [threshold]
-  nil)
-
-(defn fetch-codecov-status
-  [threshold]
-  nil)
+  (timbre/info "fetching" url)
+  (md/chain
+   (http/get url)
+   :body
+   bs/to-input-stream
+   (fn [stream]
+     (timbre/info "got status page for" url)
+     stream)))
 
 (defn fetch-github-status
   []
-  nil)
+  (timbre/info "fetching github")
+  (md/chain
+   (http/get github)
+   :body
+   bs/to-reader
+   (fn [s]
+     (timbre/info "got response from githb")
+     (json/parse-stream s true))))
 
-(defn red-alert
+(defn red-alert?
   [github codecov travis]
-  nil)
+  (not (and (= 0 (count travis))
+            (= 0 (count codecov))
+            (= "good" github))))
+
+(defn tweet
+  [message]
+  (let [{:keys [api_key api_secret access_token access_secret]} env/env
+        token (tw-auth/make-oauth-creds api_key
+                                        api_secret
+                                        access_token
+                                        access_secret)]
+    (tw-api/statuses-update :oauth-creds token
+                            :params {:status message})))
+
+(defn ugoh
+  [period]
+  (let [threshold-date (time/minus (time/now) (time/seconds period))]
+    (md/let-flow [co (fetch-statuspage codecov threshold-date)
+                  tr (fetch-statuspage travis threshold-date)
+                  gh (fetch-github-status)]
+                 (let [parsed-co (parse-status-page co threshold-date)
+                       parsed-tr (parse-status-page tr threshold-date)
+                       parsed-gh (parse-github gh)
+                       new-state (red-alert? parsed-gh parsed-co parsed-tr)
+                       old-state @state]
+                   (reset! state new-state)
+                   (cond
+                     (and (= new-state old-state) (not old-state))
+                     (timbre/info "still sunny")
+
+                     (and (= new-state old-state) old-state)
+                     (timbre/info "still dark")
+
+                     (and (not= new-state old-state) (not old-state))
+                     (do
+                       (timbre/info "sunny again")
+                       (tweet "sunny again"))
+
+                     (and (not= new-state old-state) old-state)
+                     (do
+                       (timbre/info "problem time")
+                       (tweet "problems")))))))
+
+(defn keep-checking
+  [period]
+  (mt/every
+   (* 1000 period) ;; ms -> sec
+   (fn [] (ugoh period) true)))
+
+(defn ^:private staying-alive
+  []
+  (.start (Thread. (fn [] (.join (Thread/currentThread))) "staying alive")))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  ;; check for the last 6 hours
+  (staying-alive)
+  (keep-checking (* 60 5)))
