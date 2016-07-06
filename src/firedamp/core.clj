34855cc7eb92fdc514801ef17d64e1eb77295624
Chris Wolfe <chriswwolfe@gmail.com>
2016-07-06 08:36:04 -0500

    hmm, errors aren't bubbling through

--- src/firedamp/core.clj
+++ src/firedamp/core.clj
@@ -12,14 +12,17 @@
    [manifold.time :as mt]
    [taoensso.timbre :as timbre]
    [twitter.oauth :as tw-auth]
-   [twitter.api.restful :as tw-api])
+   [twitter.api.restful :as tw-api]
+   [twitter.callbacks.handlers :as tw-handlers])
+  (:import
+   (twitter.callbacks.protocols AsyncSingleCallback))
   (:gen-class))
 
 (def github "https://status.github.com/api/status.json")
 (def codecov "http://status.codecov.io/history.atom")
 (def travis "http://www.traviscistatus.com/history.atom")
 
-(def state (atom {:alarm-state true
+(def state (atom {:alarm-state false
                   :last-update (time/now)}))
 
 (defn parse-status-page
@@ -64,32 +67,32 @@
             (= "good" github))))
 
 (defn tweet
-  [message]
+  [message token]
   (timbre/info "tweeting" message)
-  (let [{:keys [api_key api_secret access_token access_secret]} env/env
-        token (tw-auth/make-oauth-creds api_key
-                                        api_secret
-                                        access_token
-                                        access_secret)
-        result (tw-api/statuses-update :oauth-creds token
-                                       :params {:status message})
-
-        ]
-    (prn result)
+  (let [on-body (fn [response]
+                  (timbre/info "tweet response"))
+        result
+        (tw-api/statuses-update :oauth-creds token
+                                :params {:status message}
+                                :callbacks (AsyncSingleCallback.
+                                            tw-handlers/response-return-body
+                                            tw-handlers/response-throw-error
+                                            tw-handlers/exception-rethrow))]
     result))
 
 (defn ugoh
-  [period]
-  (let [threshold-date (time/minus (time/now) (time/seconds period))]
+  [ctx period]
+  (let [threshold-date (time/minus (time/now) (time/seconds period))
+        token (:token ctx)
+        old-state (:alarm-state ctx)]
     (md/let-flow [co (fetch-statuspage codecov threshold-date)
                   tr (fetch-statuspage travis threshold-date)
                   gh (fetch-github)]
       (let [parsed-co (parse-status-page co threshold-date)
             parsed-tr (parse-status-page tr threshold-date)
             parsed-gh (parse-github gh)
-            new-state (red-alert? parsed-gh parsed-co parsed-tr)
-            old-state (:alarm-state @state)]
-        (prn old-state new-state)
+            new-state (red-alert? parsed-gh parsed-co parsed-tr)]
+        (timbre/info "s0" old-state "s1" new-state)
         (cond
           (and (= new-state old-state) (not old-state))
           (timbre/info "still sunny")
@@ -99,27 +102,46 @@
 
           (and (not= new-state old-state) (not old-state))
           (do
-            (timbre/info "sunny again")
-            (tweet "sunny again"))
+            (timbre/info "problem time")
+            (md/->deferred (tweet "expect problems" token))))
 
           (and (not= new-state old-state) old-state)
           (do
-            (timbre/info "problem time")
-            (tweet "problems")))
-        (reset! state {:alarm-state state
-                       :last-update (time/now)})))))
+            (timbre/info "sunny again")
+            (md/->deferred (tweet "things are improving" token)))
+
+          ;; return some new app state
+          {:alarm-state state
+           :token token
+           :last-update (time/now)}))))
+
+(defn reset-world
+  [period]
+  (md/let-flow [new-state (ugoh @state period)]
+    (reset! state new-state)))
 
 (defn keep-checking
   [period]
   (mt/every
    (* 1000 period) ;; ms -> sec
-   (fn [] (ugoh period) true)))
+   (fn [] (reset-world period))))
 
 (defn ^:private staying-alive
   []
   (.start (Thread. (fn [] (.join (Thread/currentThread))) "staying alive")))
 
+(defn setup-twitter
+  [env]
+  (let [{:keys [api-key api-secret access-token access-token-secret]} env
+        token (tw-auth/make-oauth-creds api-key
+                                        api-secret
+                                        access-token
+                                        access-token-secret)]
+    token))
+
+
 (defn -main
   [& args]
+  (swap! state conj (setup-twitter env/env))
   (staying-alive)
   (keep-checking (* 60 2)))
