1d0348f755f4845af327da74f3303e5fa48db71f
Chris Wolfe <chriswwolfe@gmail.com>
2016-07-10 16:01:55 -0500

    bit more cleanup

--- src/firedamp/core.clj
+++ src/firedamp/core.clj
@@ -93,6 +93,7 @@
 (defn tweet!
   [message token]
   (timbre/info "tweeting" message)
+  ;; XXX this might want a timeout value
   (md/future
     (tw-api/statuses-update :oauth-creds token
                             :params {:status message})))
@@ -104,12 +105,13 @@
     ::darkening (tweet! "expect problems" token)
     ::brightening (tweet! "repaired" token)))
 
+;; it seems odd that this controls tweeting.
 (defn alert!
   [ctx parsed-statuses]
-  (let [{:keys [alarm-state token]} ctx
+  (let [{s0 :alarm-state token :token} ctx
         {:keys [codecov github travis]} parsed-statuses
-        new-alarm-state (red-alert? github codecov travis)
-        status (get-next-state alarm-state new-alarm-state)]
+        s1 (red-alert? github codecov travis)
+        status (get-next-state s0 s1)]
     (tweet-alert! token status)
     (-> ctx
         (assoc :alarm-state status)
@@ -124,7 +126,7 @@
   [period]
   (mt/every
    (* 1000 period) ;; ms -> sec
-   (fn [] (run-world! period))))
+   #(run-world! period)))
 
 (defn ^:private staying-alive
   []
